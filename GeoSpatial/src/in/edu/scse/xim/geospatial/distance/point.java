89007921c3713ef929eec5c1b00b654f3e9054d1
Ian <72592839+Ankitrout@users.noreply.github.com>
2021-12-09 11:34:23 +0530

    Update point.java

--- GeoSpatial/src/in/edu/scse/xim/geospatial/distance/point.java
+++ GeoSpatial/src/in/edu/scse/xim/geospatial/distance/point.java
@@ -8,7 +8,7 @@ public class point {
         double Y2 = b / 57.29577951;
         System.out.println("The given latitude and longitude are: " + L1 + " " + Y1);
         System.out.println("The given latitude and longitude are: " + L2 + " " + Y2);
-        double Latitude = L2 - L1;
+        double Latitude = L2 - L1; //Haversine formula
         double Longitude = Y2 - Y1;
         double D = Math.pow(Math.sin(Latitude / 2), 2)
                 + Math.cos(L1) * Math.cos(L2) * Math.pow(Math.sin(Longitude / 2), 2);
